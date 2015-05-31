28e7faa6abc144300266bff80fcb55e060ad18c8
Jonas Meinertz Hansen <jonas@meinertz.org>
2015-06-01 01:13:20 +0200

    Cleanup of text loading script

--- loadtext.py
+++ loadtext.py
@@ -2,30 +2,26 @@ import csv
 import numpy
 import scipy.sparse
 import theano
-import time
 
-def load_text(data='data/dbpedia_csv/train.csv', samples=560000, length=1014,
-              rng=numpy.random):
-    start_time = time.clock()
-    alphabet = list(
-        "abcdefghijklmnopqrstuvwxyz0123456789-,;.!?:'\"/\|_@#$%^&*~`+-=<>()[]{}"
-        )
-    permutation = rng.permutation(samples)
+def load_text(file='train.csv', samples=560000, length=130):
+    permutation = numpy.random.permutation(samples)
+    alphabet = list("abcdefghijklmnopqrstuvwxyz0123456789-,;.!?:'\"/\|_@#$%^&*~`+-=<>()[]{}")
 
-    reader = csv.reader(open(data, 'rb'))
-    y = numpy.zeros([samples], dtype=numpy.int_)
-
+    # initialize arrays for building sparce matrices
     values = numpy.zeros(samples*length, dtype=theano.config.floatX)
     rows = numpy.zeros(samples*length, dtype=numpy.int_)
     cols = numpy.zeros(samples*length, dtype=numpy.int_)
-    arrit = 0
+
+    reader = csv.reader(open('data/dbpedia_csv/'+file, 'rb'))
+    y = numpy.zeros([samples], dtype=numpy.int_)
+    arrit = 0 # for keeping track of the number of non-zero entries
     for it in permutation:
         row = reader.next()
-        y[it] = int(row[0])-1
-        #y[it][int(row[0])-1] = 1
-        string = row[1] + row[2]
+        y[it] = int(row[0])-1       # the label
+        string = row[1] + row[2]    # title + content
         string = string.lower()
 
+        # convert characters to one-hot vectors
         for idx, char in enumerate(string[0:length]):
             if char in alphabet:
                 values[arrit] = 1
@@ -33,16 +29,9 @@ def load_text(data='data/dbpedia_csv/train.csv', samples=560000, length=1014,
                 cols[arrit] = idx + alphabet.index(char)*length
                 arrit += 1
 
+    # create the sparse matrix
     x = scipy.sparse.csr_matrix((values[0:arrit],
                                  (rows[0:arrit], cols[0:arrit])),
                                 shape=(samples, length*69))
 
-    #print (x.data.nbytes + x.indices.nbytes + x.indptr.nbytes)/1000
-
-    #print '    in %.1f seconds' % (time.clock() - start_time)
-
-    return (x, y) #(xs[0], ys[0]), (xs[1], ys[1]), (xs[2], ys[2])
-
-if __name__ == "__main__":
-    (x, y) = load_text(samples=10)
-    print y
+    return (x, y)
